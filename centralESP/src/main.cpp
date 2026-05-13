a11d20719d819774da08e9ba6ff2a4bdafea1078
Vladimir Andral <andralvladimir@gmail.com>
2026-05-13 16:33:30 -0700

    Working example of ESP-NOW communication

--- centralESP/src/main.cpp
+++ centralESP/src/main.cpp
@@ -1,18 +1,38 @@
+/*
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * regenbogencode@gmail.com wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return
+ */
 #include <Arduino.h>
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif ESP32
+#include <WiFi.h>
+#endif
+#include "ESPNowW.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Red flag sticky is central
+
+// Must be the receiver's actual mac address; you can't just make one up
+uint8_t receiver_mac[] = {0xA0, 0xB7, 0x65, 0x22, 0xFF, 0x94};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(9600);
+    Serial.println("ESPNow sender Demo");
+#ifdef ESP8266
+    WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
+#elif ESP32
+    WiFi.mode(WIFI_MODE_STA);
+#endif
+    WiFi.disconnect();
+    ESPNow.init();
+    ESPNow.add_peer(receiver_mac);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    static uint8_t a = 0;
+    delay(1000);
+    ESPNow.send_message(receiver_mac, &a, 1);
+    Serial.println(a++);
 }
--- peripheralESP/src/main.cpp
+++ peripheralESP/src/main.cpp
@@ -1,18 +1,57 @@
+/*
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * regenbogencode@gmail.com wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return
+ */
 #include <Arduino.h>
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif ESP32
+#include <WiFi.h>
+#endif
+#include "ESPNowW.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Green flag sticky is peripheral
+
+/*
+    Discovered by running:
+        Serial.begin(####);
+        WiFi.mode(WIFI_MODE_STA);
+        Serial.println(WiFi.macAddress());
+*/
+uint8_t mac[] = {0xA0, 0xB7, 0x65, 0x22, 0xFF, 0x94};
+
+void onRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+    char macStr[18];
+    snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+             mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4],
+             mac_addr[5]);
+    Serial.print("Last Packet Recv from: ");
+    Serial.println(macStr);
+    Serial.print("Last Packet Recv Data: ");
+    // if it could be a string, print as one
+    if (data[data_len - 1] == 0)
+        Serial.printf("%s\n", data);
+    // additionally print as hex
+    for (int i = 0; i < data_len; i++) {
+        Serial.printf("(hex) %x", data[i]);
+    }
+    Serial.println("");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(9600);
+    Serial.println("ESPNow receiver Demo");
+#ifdef ESP8266
+    WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
+#elif ESP32
+    WiFi.mode(WIFI_MODE_STA);
+#endif
+    WiFi.disconnect();
+    ESPNow.set_mac(mac);
+    ESPNow.init();
+    ESPNow.reg_recv_cb(onRecv);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
