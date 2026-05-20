3bf3c563aa6a3ea63b11f75edeb6c366af6387cd
Vladimir Andral <andralvladimir@gmail.com>
2026-05-19 20:11:30 -0700

    Small serial change

--- RasPi3/main.py
+++ RasPi3/main.py
@@ -4,7 +4,7 @@ import serial
 # This is the pin that the Pi will used to trigger an interrupt on the ESP
 PERI_INTR_TRIG_PIN = LED("GPIO17")
 
-serObj = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+serObj = serial.Serial('/dev/serial0', 9600, timeout=1)
 serObj.write(b"Sent from Pi~")
 peri_response = serObj.readline()
 print(peri_response.decode('utf-8'))
