a8d3fc79d84a87ad2644af79aea186316ab858ae
Vladimir Andral <andralvladimir@gmail.com>
2026-05-19 20:08:39 -0700

    Init for raspi codebase:

---
+++ RasPi3/main.py
@@ -0,0 +1,13 @@
+from gpiozero import LED
+import serial
+
+# This is the pin that the Pi will used to trigger an interrupt on the ESP
+PERI_INTR_TRIG_PIN = LED("GPIO17")
+
+serObj = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+serObj.write(b"Sent from Pi~")
+peri_response = serObj.readline()
+print(peri_response.decode('utf-8'))
+
+
+
