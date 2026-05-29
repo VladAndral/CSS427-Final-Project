a9c2a2aa047b1435d63afcd42070ad63c7cc8f72
Vladimir Andral <andralvladimir@gmail.com>
2026-05-29 11:57:21 -0700

    Chaning attr name enum names

--- utils/utils.h
+++ utils/utils.h
@@ -76,9 +76,9 @@ enum Action : int
 enum Attr_Name : int
 {
     ATTR_NAME_INVALID = 0,
-    ATTR_NAME_SNS_POLL_FREQ = 1,
-    ATTR_NAME_SNS_SENSITIVITY = 2,
-    ATTR_NAME_SYS_MODE = 3
+    ATTR_NAME_POLL_FREQ = 1,
+    ATTR_NAME_SENSITIVITY = 2,
+    ATTR_NAME__MODE = 3
 };
 #define ATTR_VAL_OFFSET_SYS 2
 enum Attr_Val : int
